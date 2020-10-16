cbcb3a18d16669d9a14845bbcf693096c4ee13a9
Дмитрий Смирнов <dmitriy@Dmitrij-Smirnov.local>
2020-10-16 18:19:04 +0300

    fix

--- public/index.php
+++ public/index.php
@@ -5,6 +5,7 @@ $query = rtrim($_SERVER['QUERY_STRING'],'/');
 require '../vendor/core/Router.php';
 require '../vendor/libs/functions.php';
 
+Router::add('posts/profile',['controller'=>'Posts', 'action'=>'opener']);
 Router::add('posts/add',['controller'=>'Posts', 'action'=>'add']);
 Router::add('posts',['controller'=>'Posts', 'action'=>'index']);
 Router::add('',['controller'=>'Main', 'action'=>'index']);
